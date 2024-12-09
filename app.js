55235491291c18ccc6757f0c9e528ef9628d4f1d
Mohd-Ammar11 <mohdammar0311@gmail.com>
2024-12-09 08:03:50 +0530

    Project File Upload

--- app.js
+++ app.js
@@ -24,10 +24,6 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 
-
-
-// const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
-
 const dbUrl = process.env.ATLASDB_URL;
 
 async function main(){
@@ -56,7 +52,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
-        secret: "mysupersecretcode"
+        secret: process.env.SECRET,
     },
     touchAfter: 24 * 3600,           // interval(in seconds) between session updates
 });
@@ -68,7 +64,7 @@ store.on("error", () => {
 
 const sessionOptions = {
     store,
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     
